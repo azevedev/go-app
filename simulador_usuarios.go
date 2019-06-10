262f5882e7694fdcc3198fb813693646eb9756c5
Matheus Azevedo <azevedooliveiramatheus@gmail.com>
2019-06-10 19:13:50 -0300

    iniciando funcionalidade de simulaçao

--- simulador_usuarios.go
+++ simulador_usuarios.go
@@ -50,5 +50,51 @@ func random_atributes(users *[]Usuario){
 }
 //Recebe uma lista de 
 func random_queries(users *[]Usuario){
+	s1 := rand.NewSource(time.Now().UnixNano())
+ 	r1 := rand.New(s1)
+	for i := 0; i < len(*users); i++{
+		r := r1.Intn(11) + 1
+		switch(r){
+		 case 00:
+		 	//Calcula_Salario_aposentadoria(*(users)[i].id)
+		 	break
+		 case 01:
+			//Calcula_Idade_aposentadoria(*(users)[i].id) 
+			break
+		 case 02:
+			//Calculo_Investimento_em_previdencia_mensal(*(users)[i].id)
+			break
+		 case 03:
+		 	//LCI(*(users)[i].id)
+			break
+		 case 04:	
+			//LCA(*(users)[i].id)
+			break
+		 case 05:
+		 	//LC(*(users)[i].id)
+		 	break
+	 	 case 06:
+	 	 	//CDB(*(users)[i].id)
+		 	break
+		 case 07:
+		 	//Acoes_Escriturais(*(users)[i].id)
+		 	break
+		 case 8:
+		 	//Rentabilidade_da_carteira(*(users)[i].id)
+		 	break	
+		 case 9:
+		 	//Rentabilidade_da_carteira(*(users)[i].id)
+		 	break
+		 case 10:
+		 	//Simulador_de_investimento(*(users)[i].id)
+		 	break
+		 case 11:
+		 	//Calculo_poupanca(*(users)[i].id)
+		 	break
 
+
+	
+}
+		 
+		}
 }
