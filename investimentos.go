845ecd7109a1a71e3909ddd7f3cd029ee2586c4d
MatheusUNB <alextexugo@gmail.com>
2019-06-23 19:23:36 -0300

    atualização

--- investimentos.go
+++ investimentos.go
@@ -1,25 +1,5 @@
 package main
 
-import "fmt"
+func simulaInvestimento() {
 
-func menu_investimentos() {
-	fmt.Println("\n***********************************************")
-	fmt.Println("***              INVESTIMENTOS              ***")
-	fmt.Println("***********************************************\n")
-
-	nome := "Matheus"
-	//idade := 26
-
-	fmt.Println("Olá sr.", nome, "faça sua escolha:\n")
-
-	fmt.Println("1- Simulador de Investimento")
-	fmt.Println("2- Análise de Perfil do Investidor")
-	fmt.Println("0- Sair do Programa")
-
-	//switch opcao
-}
-
-func investimentos() {
-
-	menu_investimentos()
 }
